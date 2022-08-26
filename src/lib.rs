dc39d16ce614402ac58df38523d52f2a072f1f93
BlankZhu <BlankZhu47@gmail.com>
2022-08-26 17:07:23 +0800

    update: remove build script & fix: v1.14 export error

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[cfg(feature = "v1_10")] mod v1_10;
+#[cfg(feature = "v1_10")] pub use self::v1_10::*;
+
+#[cfg(feature = "v1_11")] mod v1_11;
+#[cfg(feature = "v1_11")] pub use self::v1_11::*;
+
+#[cfg(feature = "v1_12")] mod v1_12;
+#[cfg(feature = "v1_12")] pub use self::v1_12::*;
+
+#[cfg(feature = "v1_13")] mod v1_13;
+#[cfg(feature = "v1_13")] pub use self::v1_13::*;
+
+#[cfg(feature = "v1_14")] mod v1_14;
+#[cfg(feature = "v1_14")] pub use self::v1_14::*;
